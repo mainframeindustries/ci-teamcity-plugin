b9ab5e9bc9f5bb65e372992850b3e87b7e57c98c
Kristján Valur Jónsson <sweskman@gmail.com>
2026-01-21 14:02:20 +0000

    Add support for job-level parameter overrides
    - Modified ProjectHandler to query project parmeter provider.
    - Allows individual jobs to override datadog.ci.enabled, datadog.ci.api.key, and datadog.ci.site
    - Maintains backwards compatibility with existing project-level configurations
    
    This enables more granular control over plugin behavior.
    
    re-add getApiKey(), use ValueResolver
    
    use build-aware  value resolver for the api key
    
    use evaluation for all parameters
    
    remove unused getProject method
    
    fallback to project resolver for passwords

--- datadog-ci-integration-server/src/main/java/jetbrains/buildServer/com/datadog/teamcity/plugin/ProjectHandler.java
+++ datadog-ci-integration-server/src/main/java/jetbrains/buildServer/com/datadog/teamcity/plugin/ProjectHandler.java
@@ -10,13 +10,8 @@ package jetbrains.buildServer.com.datadog.teamcity.plugin;
 import com.intellij.openapi.diagnostic.Logger;
 import jetbrains.buildServer.parameters.ProcessingResult;
 import jetbrains.buildServer.parameters.ValueResolver;
-import jetbrains.buildServer.serverSide.ProjectManager;
 import jetbrains.buildServer.serverSide.SBuild;
-import jetbrains.buildServer.serverSide.impl.ProjectEx;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Nonnull;
-import java.util.Optional;
 
 import static java.lang.String.format;
 
@@ -30,60 +25,66 @@ public class ProjectHandler {
     protected static final String DATADOG_ENABLED_PARAM = "datadog.ci.enabled";
     protected static final String DATADOG_ENABLE_NON_COMPOSITE_PARAM = "datadog.ci.enable.non-composite";
 
-    private final ProjectManager projectManager;
-
-    public ProjectHandler(ProjectManager projectManager) {
-        this.projectManager = projectManager;
-    }
-
     public ProjectParameters getProjectParameters(SBuild build) {
-        ProjectEx project = getProject(build);
-        String apiKey = getApiKey(project);
-        String ddSite = project.getParameterValue(DATADOG_SITE_PARAM);
-
-        if (ddSite == null) {
-            throw new IllegalArgumentException(
-                    format("Could not find required property '%s' for project '%s'. Project parameters: %s",
-                            DATADOG_SITE_PARAM, project.getName(), project.getParameters()));
-        }
-
+        String apiKey = getBuildParameter(build, DATADOG_API_KEY_PARAM);
+        String ddSite = getBuildParameter(build, DATADOG_SITE_PARAM);
         return new ProjectParameters(apiKey, ddSite);
     }
 
     public boolean isPluginEnabled(SBuild build) {
-        ProjectEx project = getProject(build);
-        String enabled = project.getParameterValue(DATADOG_ENABLED_PARAM);
+        String enabled = getBuildParameter(build, DATADOG_ENABLED_PARAM, null);
         boolean isPluginEnabled = Boolean.parseBoolean(enabled);
         if (!isPluginEnabled) {
-            LOG.debug(format("Plugin not enabled in project '%s'", project.getFullName()));
+            LOG.debug(format("Plugin not enabled for build %s", build.getBuildId()));
         }
 
         return isPluginEnabled;
     }
 
     public boolean isNonCompositeEnabled(SBuild build) {
-        String enabled = build.getParametersProvider().get(DATADOG_ENABLE_NON_COMPOSITE_PARAM);
+        String enabled = getBuildParameter(build, DATADOG_ENABLE_NON_COMPOSITE_PARAM, null);
         return Boolean.parseBoolean(enabled);
     }
 
-    @Nonnull
-    private ProjectEx getProject(SBuild build) {
-        return (ProjectEx) Optional.ofNullable(build.getProjectId())
-            .map(projectManager::findProjectById)
-            .orElse(projectManager.getRootProject());
-    }
-
-    private String getApiKey(ProjectEx project) {
-        String apiKeyReference = String.format("%%%s%%", DATADOG_API_KEY_PARAM);
-        ValueResolver resolver = project.getValueResolver();
-        ProcessingResult resolved = resolver.resolve(apiKeyReference);
-        if (!resolved.isFullyResolved()) {
+    private String getBuildParameter(SBuild build, String parameterName) {
+        // Check parameter exists first
+        String value = build.getParametersProvider().get(parameterName);
+        if (value == null) {
             throw new IllegalArgumentException(
-                    format("Could not find required property '%s' for project '%s'. Project parameters: %s",
-                            DATADOG_API_KEY_PARAM, project.getName(), project.getParameters()));
+                    format("Could not find required property '%s' for build %s",
+                            parameterName, build.getBuildId()));
         }
 
-        return resolved.getResult();
+        return resolveBuildParameter(build, parameterName);
+    }
+
+    private String getBuildParameter(SBuild build, String parameterName, String defaultValue) {
+        // Check if parameter exists
+        String value = build.getParametersProvider().get(parameterName);
+        if (value == null) {
+            return defaultValue;
+        }
+
+        return resolveBuildParameter(build, parameterName);
+    }
+
+    private String resolveBuildParameter(SBuild build, String parameterName) {
+        // Resolve any %foo% references or passwords in parameters
+        // Try build's ValueResolver first to support job-level overrides
+        String paramReference = String.format("%%%s%%", parameterName);
+        ValueResolver buildResolver = build.getValueResolver();
+        ProcessingResult resolved = buildResolver.resolve(paramReference);
+        String result = resolved.getResult();
+        
+        // If the value is scrambled (password parameter), fall back to project resolver
+        // Build resolver doesn't have access to unscramble passwords
+        if ("*******".equals(result)) {
+            ValueResolver projectResolver = build.getBuildType().getProject().getValueResolver();
+            resolved = projectResolver.resolve(paramReference);
+            result = resolved.getResult();
+        }
+        
+        return result;
     }
 
     public static class ProjectParameters {
