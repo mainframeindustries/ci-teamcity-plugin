d4c44339d74d90306ba2c77b1d17da57aed9e612
Kristján Valur Jónsson <sweskman@gmail.com>
2026-01-23 13:56:52 +0000

    better logging on reasons why build is ignored

--- datadog-ci-integration-server/src/main/java/jetbrains/buildServer/com/datadog/teamcity/plugin/DatadogServerAdapter.java
+++ datadog-ci-integration-server/src/main/java/jetbrains/buildServer/com/datadog/teamcity/plugin/DatadogServerAdapter.java
@@ -56,8 +56,9 @@ public class DatadogServerAdapter extends BuildServerAdapter {
             return;
         }
 
-        if (!isProcessableBuild(build)) {
-            LOG.info(format("Ignoring build with id '%s' and name '%s'", build.getBuildId(), buildName(build)));
+        String ignoreReason = getIgnoreReason(build);
+        if (ignoreReason != null) {
+            LOG.info(format("Ignoring build '%s' (id: %s): %s", buildName(build), build.getBuildId(), ignoreReason));
             return;
         }
 
@@ -72,22 +73,25 @@ public class DatadogServerAdapter extends BuildServerAdapter {
         buildChainProcessor.process(pipelineBuild);
     }
 
-    private boolean isProcessableBuild(SBuild build) {
+    /**
+     * Returns a reason why the build should be ignored, or null if it should be processed.
+     */
+    private String getIgnoreReason(SBuild build) {
         // Personal builds are never processed
         if (build.isPersonal()) {
-            return false;
+            return "personal build";
         }
 
         // Must be the final build in the chain (no dependents)
         if (build.getBuildPromotion().getNumberOfDependedOnMe() != 0) {
-            return false;
+            return "not a final build in chain (has dependents)";
         }
 
         // If it's a non-composite build, reject if feature is not enabled
         if (!build.isCompositeBuild() && !projectHandler.isNonCompositeEnabled(build)) {
-            return false;
+            return "non-composite build and feature not enabled";
         }
 
-        return true;
+        return null;
     }
 }
